82602a9ef2acc230264147909901249a99785ca1
tinaimitchell23 <43891889+tinaimitchell23@users.noreply.github.com>
2018-12-21 11:01:56 -0600

    Delete GadgetInterface.java

--- src/com/java/project/GadgetInterface.java
+++
@@ -1,7 +0,0 @@
-package com.java.project;
-
-public interface GadgetInterface {
-	public enum Color {UNPAINTED, PAINTED, GOLD, SILVER, BRONZE };
-	public void paint(GadgetInterface.Color color);
-	public float getPrice();
-}
